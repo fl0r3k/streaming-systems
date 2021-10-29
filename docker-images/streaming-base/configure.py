41242f559b9bb6181d71dadeda42e4e6147b7239
Piotr Florczyk <pflorczy@gmail.com>
2021-10-29 12:12:11 +0200

    Preparing base image

---
+++ docker-images/streaming-base/configure.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from jinja2 import Environment, FileSystemLoader
+
+
+def generate_config(template_file, output_file):
+  try:
+    j2env = Environment(
+      loader=FileSystemLoader(searchpath="."),
+      trim_blocks=True
+    )
+    with open(output_file, 'w') as f:
+      template = j2env.get_template(template_file)
+      f.write(template.render(env=os.environ))
+    return True
+
+  except Exception as e:
+    print(e, file=sys.stderr)
+    return False
+
+
+if __name__ == '__main__':
+  if len(sys.argv) >= 2:
+    template_file = sys.argv[1]
+    output_file = sys.argv[2]
+    generate_config(template_file,output_file)
