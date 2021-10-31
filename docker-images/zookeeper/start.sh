5cc6e1b6d34865b352e26c434048ea947759a1bd
Piotr Florczyk <pflorczy@gmail.com>
2021-10-31 13:03:26 +0100

    Added Zookeeper image

---
+++ docker-images/zookeeper/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+config_template=${ZOOKEEPER_HOME}/conf/zoo.cfg.template
+config_file=${ZOOKEEPER_HOME}/conf/zoo.cfg
+
+python3 configure.py ${config_template} ${config_file}
+
+zkServer.sh start
+tail -f zookeeper.out
