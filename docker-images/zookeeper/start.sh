ba3dc7ebe49c9154b072bf29a12bb89ad2080696
Piotr Florczyk <pflorczy@gmail.com>
2021-11-18 20:02:41 +0100

    Zookeeper image improvements for cluster support

--- docker-images/zookeeper/start.sh
+++ docker-images/zookeeper/start.sh
@@ -5,4 +5,6 @@ config_file=${ZOOKEEPER_HOME}/conf/zoo.cfg
 
 python3 configure.py ${config_template} ${config_file}
 
+echo ${ZOOKEEPER_SERVER_ID} > ${ZOOKEEPER_DATA_DIR}/myid
+
 zkServer.sh start-foreground
